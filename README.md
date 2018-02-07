419f447814a79ee05ee54b42ca7cd68c54db0a8b
ssh <shensh@ddd.online>
2018-02-07 18:04:27 +0800

    change style

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Roguelike
 2D Voxel level roguelike game
 
+2018/2/6
+http://www.codingcookies.com/2013/04/01/building-a-roguelike-in-javascript-part-1/
+
 2018/2/7
 http://www.codingcookies.com/2013/04/03/building-a-roguelike-in-javascript-part-2/
--- assets/game.js
+++ assets/game.js
@@ -7,21 +7,9 @@ window.onload = function() {
   if (!ROT.isSupported()) {
     alert('The rot.js library isn\'t supported by your browser.');
   } else {
-    let game = new Game();
-    game.init();
 
-    document.body.appendChild(game.getDisplay().getContainer());
   }
 };
 
-let Game = function() {
-  this.display = null;
-  
-  this.init = function() {
-    this.display = new ROT.Display({width: 80, height: 24});
-  };
-  
-  this.getDisplay = function() {
-    return this.display;
-  };
-};
+
+
