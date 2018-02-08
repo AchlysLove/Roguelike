d897b012d55afbb0610532eb82a8da69e08987d1
ssh <shensh@ddd.online>
2018-02-08 15:11:36 +0800

    part 2 completed

--- assets/game.js
+++ assets/game.js
@@ -7,8 +7,52 @@ window.onload = function() {
   if (!ROT.isSupported()) {
     alert('The rot.js library isn\'t supported by your browser.');
   } else {
+    Game.init();
 
+    document.body.appendChild(Game.getDisplay().getContainer());
+
+    Game.switchScreen(Game.Screen.startScreen);
   }
+};
+
+let Game = {
+  display: null,
+  currentScreen: null,
+
+  init() {
+    this.display = new ROT.Display({width: 80, height: 20});
+
+    let instance = this;
+    let bindEventToScreen = function(event) {
+      window.addEventListener(event, (e) => {
+        if (instance.currentScreen !== null) {
+          instance.currentScreen.handleInput(event, e);
+        }
+      });
+    };
+
+    bindEventToScreen('keydown');
+    bindEventToScreen('keyup');
+    bindEventToScreen('keypress');
+  },
+
+  getDisplay() {
+    return this.display;
+  },
+
+  switchScreen(screen) {
+    if (this.currentScreen !== null) {
+      this.currentScreen.exit();
+    }
+
+    this.getDisplay().clear();
+    this.currentScreen = screen;
+
+    if (this.currentScreen !== null) {
+      this.currentScreen.enter();
+      this.currentScreen.render(this.display);
+    }
+  },
 };
 
 
--- assets/screen.js
+++ assets/screen.js
@@ -2,3 +2,95 @@
  * Created by Shihao Shen on 2018/2/7
  */
 
+Game.Screen = {};
+
+Game.Screen.templateScreen = {
+  enter() {
+
+  },
+
+  exit() {
+
+  },
+
+  render(display) {
+
+  },
+
+  handleInput(inputType, inputData) {
+
+  },
+};
+
+Game.Screen.startScreen = {
+  enter() {
+
+  },
+
+  exit() {
+
+  },
+
+  render(display) {
+    display.drawText(1, 1, '%c{yellow}Javascript Roguelike');
+    display.drawText(1, 2, 'Press [Enter] to start!');
+  },
+
+  handleInput(inputType, inputData) {
+    if (inputType === 'keydown') {
+      if (inputData.keyCode === ROT.VK_RETURN) {
+        Game.switchScreen(Game.Screen.playScreen);
+      }
+    }
+  },
+};
+
+Game.Screen.playScreen = {
+  enter() {
+
+  },
+
+  exit() {
+
+  },
+
+  render(display) {
+    display.drawText(3, 5, '%c{red}%b{white}This game is so much fun!');
+    display.drawText(4, 6, 'Press [Enter] to win, or [Esc] to lose!');
+  },
+
+  handleInput(inputType, inputData) {
+    if (inputType === 'keydown') {
+      if (inputData.keyCode === ROT.VK_RETURN) {
+        Game.switchScreen(Game.Screen.winScreen);
+      } else if (inputData.keyCode === ROT.VK_ESCAPE) {
+        // Game.switchScreen(Game.Screen.loseScreen);
+      }
+    }
+  },
+};
+
+Game.Screen.winScreen = {
+  enter() {
+
+  },
+
+  exit() {
+
+  },
+
+  render(display) {
+    for (let i = 0; i < 22; i++) {
+      // Generate random background colors
+      let r = Math.round(Math.random() * 255);
+      let g = Math.round(Math.random() * 255);
+      let b = Math.round(Math.random() * 255);
+      let background = ROT.Color.toRGB([r, g, b]);
+      display.drawText(2, i + 1, '%b{' + background + '}You win!');
+    }
+  },
+
+  handleInput(inputType, inputData) {
+
+  },
+};
